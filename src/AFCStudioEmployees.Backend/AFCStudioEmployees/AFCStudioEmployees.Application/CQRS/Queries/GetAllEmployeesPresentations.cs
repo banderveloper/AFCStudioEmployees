abf79a3d2f0bf4b384a273ad700f77915dc13e2c
banderveloper <kpssdeveloper@gmail.com>
2024-05-05 20:53:51 +0300

    feat(backend): add getting all employees endpoint

---
+++ src/AFCStudioEmployees.Backend/AFCStudioEmployees/AFCStudioEmployees.Application/CQRS/Queries/GetAllEmployeesPresentations.cs
@@ -0,0 +1,40 @@
+﻿using AFCStudioEmployees.Application.DTO;
+using AFCStudioEmployees.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace AFCStudioEmployees.Application.CQRS.Queries;
+
+public class GetAllEmployeesPresentationsQuery : IRequest<Result<IEnumerable<EmployeePresentationDTO>>>
+{
+}
+
+public class GetAllEmployeesPresentationsQueryHandler : IRequestHandler<GetAllEmployeesPresentationsQuery,
+    Result<IEnumerable<EmployeePresentationDTO>>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetAllEmployeesPresentationsQueryHandler(IApplicationDbContext context)
+        => _context = context;
+
+    public async Task<Result<IEnumerable<EmployeePresentationDTO>>> Handle(GetAllEmployeesPresentationsQuery request,
+        CancellationToken cancellationToken)
+    {
+        var result = await _context.Employees
+            .Include(employee => employee.Department)
+            .Include(employee => employee.Job)
+            .Select(employee => new EmployeePresentationDTO
+            {
+                EmployeeId = employee.Id,
+                LastName = employee.LastName,
+                FirstName = employee.FirstName,
+                MiddleName = employee.MiddleName,
+                BirthDate = employee.Birthdate,
+                DepartmentId = employee.DepartmentId,
+                EmployeeSalary = employee.Job.Salary,
+                EmployeeInviteTime = employee.CreatedAt
+            }).ToListAsync(cancellationToken);
+
+        return Result<IEnumerable<EmployeePresentationDTO>>.Success(result);
+    }
+}
---
+++ src/AFCStudioEmployees.Backend/AFCStudioEmployees/AFCStudioEmployees.WebAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using AFCStudioEmployees.Application;
+using AFCStudioEmployees.Application.CQRS.Queries;
+using AFCStudioEmployees.Application.DTO;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AFCStudioEmployees.WebAPI.Controllers;
+
+/// <summary>
+/// Controller for employees
+/// </summary>
+[ApiController]
+[Route("employees")]
+public class EmployeesController
+{
+    private readonly IMediator _mediator;
+
+    public EmployeesController(IMediator mediator)
+        => _mediator = mediator;
+
+    /// <summary>
+    /// Get all employees without pagination
+    /// </summary>
+    /// <returns>Result with list of employees</returns>
+    [HttpGet]
+    public async Task<Result<IEnumerable<EmployeePresentationDTO>>> GetAllEmployees()
+        => await _mediator.Send(new GetAllEmployeesPresentationsQuery());
+}
