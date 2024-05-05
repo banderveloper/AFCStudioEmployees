b7760c68295bba56001da27076195f2cacc0b00d
banderveloper <kpssdeveloper@gmail.com>
2024-05-05 18:30:21 +0300

    feat(backend): fill program.cs and first server start

--- src/AFCStudioEmployees.Backend/AFCStudioEmployees/AFCStudioEmployees.WebAPI/Program.cs
+++ src/AFCStudioEmployees.Backend/AFCStudioEmployees/AFCStudioEmployees.WebAPI/Program.cs
@@ -1,6 +1,49 @@
+using AFCStudioEmployees.Application;
+using AFCStudioEmployees.Application.Converters;
+using AFCStudioEmployees.Persistence;
+using AFCStudioEmployees.WebAPI;
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddCustomConfiguration();
+
+// Inject other layers
+builder.Services
+    .AddApplication()
+    .AddPersistence(builder.Environment.EnvironmentName.ToLower());
+
+var scope = builder.Services.BuildServiceProvider().CreateScope();
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.WithOrigins("*", "null", "http://localhost:5000", "http://localhost:3000")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+    });
+});
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    // ErrorCode enum int value to snake_case_string in response (ex: not 1, but username_already_exists)
+    options.JsonSerializerOptions.Converters.Add(new SnakeCaseStringEnumConverter<ErrorCode>());
+});
+
+// Initialize database if it is not exists
+var applicationDbContext = scope.ServiceProvider.GetService<ApplicationDbContext>();
+DatabaseInitializer.Initialize(applicationDbContext);
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 app.MapGet("/", () => "Hello World!");
 
+Console.WriteLine("Server started");
 app.Run();
