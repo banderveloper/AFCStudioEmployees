76dadc601c62f1e859a394189e52f4cf61ebd05b
banderveloper <kpssdeveloper@gmail.com>
2024-05-05 19:54:19 +0300

    feat(backend): add getting jobs and departments previews

---
+++ src/AFCStudioEmployees.Backend/AFCStudioEmployees/AFCStudioEmployees.Application/DTO/DepartmentPreviewDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace AFCStudioEmployees.Application.DTO;
+
+public class DepartmentPreviewDTO
+{
+    public long DepartmentId { get; set; }
+    public string DepartmentName { get; set; }
+}
---
+++ src/AFCStudioEmployees.Backend/AFCStudioEmployees/AFCStudioEmployees.Application/DTO/JobPreviewDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace AFCStudioEmployees.Application.DTO;
+
+public class JobPreviewDTO
+{
+    public long JobId { get; set; }
+    public string JobName { get; set; }
+}
