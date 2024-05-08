b9fd2f5af87e4105381b1a13da3f6bf6957df3ad
banderveloper <kpssdeveloper@gmail.com>
2024-05-08 09:30:46 +0300

    feat(frontend): add endpoints and axios instance with interceptors

---
+++ src/AFCStudioEmployees.Frontend/src/shared/axios.ts
@@ -0,0 +1,29 @@
+import axios, {AxiosInstance} from "axios";
+import {API_URL} from "./endpoints.ts";
+
+const api : AxiosInstance = axios.create({
+    baseURL: API_URL,
+    withCredentials: false
+});
+
+api.interceptors.request.use(request => {
+
+    console.log('[INTERCEPTOR] REQUEST');
+    console.log(request);
+
+    return request;
+});
+
+api.interceptors.response.use(response => {
+
+    console.log('[INTERCEPTOR] RESPONSE');
+    console.log(response);
+
+    return response;
+}, async error => {
+
+    console.error('[INTERCEPTOR] RESPONSE ERROR');
+    console.error(error);
+
+    return error;
+});
